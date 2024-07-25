8ec684ef34ea81a336af84c93c89ec5b1b854bc7
shruti9934 <shrutisharmajsr2003@gmail.com>
2024-07-25 20:24:04 +0530

    Update splashScreen.js

--- Screens/splashScreen/splashScreen.js
+++ Screens/splashScreen/splashScreen.js
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-
+import { View, StyleSheet, Image,Text } from 'react-native';
 const SplashScreen = () => {
   const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowSplash(false);
-    }, 4000); // 4 seconds
+    }, 6000); // 5 seconds
 
     return () => clearTimeout(timeoutId);
   }, []);
 
   return (
     <View style={styles.container}>
-      {showSplash && (
-        <Image
-          source={require('../../assets/google.png')} // Replace with your actual image
-          style={styles.splashImage}
-        />
-      )}
-    </View>
-  );
+    {showSplash && (
+      <Image
+        source={require("../../../../assets/images/zyngal.png")}
+        style={styles.splashImage}
+      />
+    )}
+  </View>
+);
 };
 
 const styles = StyleSheet.create({
@@ -32,9 +31,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   splashImage: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: 300,
+    height: 300,
+    
   },
 });
 
